bdf7f34483b83032000b59be341c946ac507f90c
vedantkubal008-bit <vedantkubal008@gmail.com>
2026-07-07 12:00:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let code="";
+
+function makeCaptcha(){
+
+const chars="ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789";
+
+code="";
+
+for(let i=0;i<6;i++){
+
+code+=chars[Math.floor(Math.random()*chars.length)];
+
+}
+
+document.getElementById("captcha").innerHTML=code;
+
+}
+
+makeCaptcha();
+
+document.getElementById("refresh").onclick=makeCaptcha;
+
+document.getElementById("submit").onclick=function(){
+
+let entered=document.getElementById("input").value;
+
+if(entered===code){
+
+document.body.innerHTML=`
+<img src="result.jpg"
+style="
+width:100vw;
+height:100vh;
+object-fit:contain;
+background:white;
+">
+`;
+
+}else{
+
+document.getElementById("msg").innerHTML="Incorrect CAPTCHA";
+
+makeCaptcha();
+
+}
+
+}
